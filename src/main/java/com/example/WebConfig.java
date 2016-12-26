e587a5c2ec020a4f4bfcce1c5a5a01a34848e802
Stuart McGregor <stumacsolutions@gmail.com>
2016-12-26 14:40:03 +0000

    Removing redundant login page.

--- src/main/java/com/example/WebConfig.java
+++ src/main/java/com/example/WebConfig.java
@@ -10,6 +10,5 @@ class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
-        registry.addViewController("/login").setViewName("login");
     }
 }
